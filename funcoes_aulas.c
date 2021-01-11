a2c4494cc7a5d8252fef5f5ff926882be9cef7ca
Mariana Pereira <2200679@my.ipleiria.pt>
2021-01-11 17:13:34 +0000

    funcao aula

--- funcoes_aulas.c
+++ funcoes_aulas.c
@@ -97,7 +97,7 @@ tipoAula *acrescentaAula(tipoAula vAula[], int *num, tipoUc vetorUc[], int posUc
                 quantHorasUc = (quantHorasUc*60); //passa horas para minutos
                 duracaoUcRest = quantHorasUc - duracaoUc; //faz a redução
                 duracaoUcRest = duracaoUcRest/60; //passa minutos para horas
-                printf("\n duracao restante: %d",duracaoUcRest);
+                printf("\n Numero de horas restante na UC %s: %d",vetorUc[posUc].designacao,duracaoUcRest);
 
             //}while(hora < 8 || hora > 18);
 
@@ -125,7 +125,7 @@ tipoAula *acrescentaAula(tipoAula vAula[], int *num, tipoUc vetorUc[], int posUc
                 quantHorasUc = (quantHorasUc*60); //passa horas para minutos
                 duracaoUcRest = quantHorasUc - duracaoUc; //faz a redução
                 duracaoUcRest = duracaoUcRest/60; //passa minutos para horas
-                printf("\n duracao restante de aulas: %d",duracaoUcRest);
+                printf("\n Numero de horas restante na UC %s: %d",vetorUc[posUc].designacao,duracaoUcRest);
 
             //}while(hora < 18 || hora > 24);
 
